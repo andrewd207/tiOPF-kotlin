8fbe6bdd39a3cc97c56f0eee92dbc13711d17ab2
andrew <andrewd207@aol.com>
2020-01-15 20:29:15 -0500

    registered a few more mediators

--- javaFXMediators/Register.kt
+++ javaFXMediators/Register.kt
@@ -5,4 +5,8 @@ import tiOPF.TypeKind
 
 fun registerFallbackMediators(){
     MediatorManager.registerMediator(TextFieldMediatorView::class, Object::class, setOf(TypeKind.STRING, TypeKind.INT, TypeKind.FLOAT))
+    MediatorManager.registerMediator(TextAreaMediatorView::class, Object::class, setOf(TypeKind.STRING))
+    MediatorManager.registerMediator(ComboBoxMediatorView::class, Object::class, setOf(TypeKind.STRING))
+    MediatorManager.registerMediator(CheckBoxMediatorView::class, Object::class, setOf(TypeKind.BOOLEAN))
+    MediatorManager.registerMediator(SpinnerMediatorViewInt::class, Object::class, setOf(TypeKind.INT))
 }
